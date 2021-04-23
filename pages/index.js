368d62612423e9f0bdcafd9b682d6b2cd050d0dd
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-04-23 23:27:16 +0430

    adding other components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1>Home Page</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus
@@ -17,6 +20,7 @@ export default function Home() {
         deserunt, harum quisquam, alias esse vero porro voluptate vel aliquam.
         Totam, dolorum.
       </p>
+      <Footer />
     </>
   );
 }
