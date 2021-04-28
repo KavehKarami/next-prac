42bb648fb1db021f0c28946862f029fc1d4b3248
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-04-28 16:54:03 +0430

    adding styles

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-import Head from "next/head";
 import Link from "next/link";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <h1 className={styles.title}>Home Page</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus
         distinctio facere quo sit in tenetur mollitia repudiandae inventore,
@@ -21,7 +19,9 @@ export default function Home() {
         Totam, dolorum.
       </p>
 
-      <Link href="/ninjas">See Ninja Listing</Link>
+      <Link href="/ninjas">
+        <span className={styles.btn}>See Ninja Listing</span>
+      </Link>
     </>
   );
 }
